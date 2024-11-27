ca7f2ff7f056c877d19fe1eae9a2d1e0632a4b54
404lucas <404lucas.ferreira@gmail.com>
2024-11-27 15:01:01 -0300

    live chat

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+//CHARLES CHEVERS 0.1.4
+
+const readline = require('readline'); //Importando readline
 const express = require('express'); //Importando Express
 const { GoogleGenerativeAI } = require("@google/generative-ai"); //Importando pacote do Google AI
 
 const app = express(); //Instanciando express
 const API_KEY = 'AIzaSyCC4QcO-okFdcecWZBeKiQejBOz83K6T2M'; //Definindo chave de API
-const AI = new GoogleGenerativeAI(API_KEY); //Novo objeto de AI
-const model = AI.getGenerativeModel({ model: "gemini-1.5-flash" }); //Pegar modelo
+const AI = new GoogleGenerativeAI(API_KEY); //Instanciando Google AI
+const model = AI.getGenerativeModel({ model: "gemini-1.5-flash" }); //Gerar modelo de texto
 
-const prompt = "gere um texto de 10 parágrafos sobre fome na áfrica, por favor";
+//Instanciando readline para chat via console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-(async () => {
-    const result = await model.generateContentStream(prompt);
+//Criando um chat com o modelo, sem histórico
+const chat = model.startChat({ history: [] });
+async function handleChat() { //Nova função para chat, com pergunta e resposta do usuário
+    rl.question("Usuário: ", async (userInput) => { //Faz uma pergunta via rlcom o input do usuário
+        try {
+            ///Manda no chat do Gemini e printa a response
+            const result = await chat.sendMessage(userInput);
+            console.log(`Charles: ${result.response.text()}`);
+            
+            //Repete a função
+            handleChat();
+        } catch (err) {
+            console.error("Erro ao processar chat: ", err);
+            rl.close();
+        }
+    });
+}
 
-    // Print text as it comes in.
-    for await (const chunk of result.stream) {
-        const chunkText = chunk.text();
-        process.stdout.write(chunkText);
-    }
-    //     const result = await model.generateContent(prompt);
-    //     console.log(result.response.candidates[0].content.parts[0].text);
-})();
-
+app.listen(3000, () => {
+    console.log(`Chat iniciado`);
+    handleChat();
+});
