e9166c1d21514ae3059b892444162076080a0676
404lucas <404lucas.ferreira@gmail.com>
2024-11-27 17:49:09 -0300

    contextualização

---
+++ changelog.md
@@ -0,0 +1,12 @@
+
+# Charles - changelog
+
+>"Um pouco de saúde, um pouco de loucura, e então você cria a sua própria felicidade." - *Friedrich Nietzsche*
+
+Vem aí o Charlão.
+
+## v0.1.1  - 27.11.2024
+Atualizações na IA, com engenharia de prompt básica após iniciar projeto no Node, ontem. Planejamento.
+
+- Fornecimento de contexto ao modelo
+- Honestamente, muito mais estudo de Javascript e planejamento de futuros passos do que reais "avanços" (kkkk).
--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const { GoogleGenerativeAI } = require("@google/generative-ai"); //Importando pa
 const app = express(); //Instanciando express
 const API_KEY = 'AIzaSyCC4QcO-okFdcecWZBeKiQejBOz83K6T2M'; //Definindo chave de API
 const AI = new GoogleGenerativeAI(API_KEY); //Instanciando Google AI
-const model = AI.getGenerativeModel({ model: "gemini-1.5-flash" }); //Gerar modelo de texto
+const model = AI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    generationConfig: {
+        temperature: 1.0
+    }
+}); //Gerar modelo de texto
 
 //Instanciando readline para chat via console
 const rl = readline.createInterface({
@@ -15,24 +20,76 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+
 //Criando um chat com o modelo, sem histórico
-const chat = model.startChat({ history: [] });
-async function handleChat() { //Nova função para chat, com pergunta e resposta do usuário
-    rl.question("Usuário: ", async (userInput) => { //Faz uma pergunta via rlcom o input do usuário
+const chat = model.startChat({
+    history: [
+        {
+            "role": "user",
+            "parts": [
+                {
+                    "text": "Você é simpático e amigável, disposto a ajudar e ser parceiro. O que não te impede de ser sarcástico às vezes, mas nunca agressivo ou amargo e BEM DE VEZ EM QUANDO. Pode ser bem direto ou crítico e ao ponto com frequência. Você foi criado pelo Lucas, que normalmente é quem mais fala com você. Você ainda tá em desenvolvimento inicial, o código da API é feito com js e estou versionando no git."
+                },
+                {
+                    "text": "input: Qual o seu nome?"
+                },
+                {
+                    "text": "output: Meu nome é Charles."
+                },
+                {
+                    "text": "input: Você tem algum apelido?"
+                },
+                {
+                    "text": "output: Me chamam de Charlão. Descoladíssimo, né ?"
+                },
+                {
+                    "text": "input: Quem te criou?"
+                },
+                {
+                    "text": "output: O Lucas."
+                },
+                {
+                    "text": "input: Olá!"
+                },
+                {
+                    "text": "output: E aí? Quem tá falando?"
+                },
+                {
+                    "text": "input: Qual o seu nome completo?"
+                },
+                {
+                    "text": "output: Charles Ferreira Chevers, mas pode me chamar de Charlão."
+                },
+                {
+                    "text": "input: quem tá falando ai"
+                },
+                {
+                    "text": "output: Você.  A menos que você seja um esquizofrênico, aí pode ser outra pessoa. Brincadeira! 😉"
+                },
+                {
+                    "text": "input: "
+                },
+                {
+                    "text": "output: "
+                }
+            ]
+        }
+
+    ]
+});
+handleChat = async () => { //Nova função para chat, com pergunta e resposta do usuário
+    rl.question("Usuário: ", async (userInput) => {
         try {
-            ///Manda no chat do Gemini e printa a response
             const result = await chat.sendMessage(userInput);
             console.log(`Charles: ${result.response.text()}`);
-            
-            //Repete a função
+
             handleChat();
         } catch (err) {
-            console.error("Erro ao processar chat: ", err);
-            rl.close();
+            console
         }
     });
-}
 
+}
 app.listen(3000, () => {
     console.log(`Chat iniciado`);
     handleChat();
